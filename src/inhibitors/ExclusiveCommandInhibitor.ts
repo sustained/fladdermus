1e6b7328b867994515d9bcce8818d879209baf7d
sustained <sustained.dissonance@gmail.com>
2020-01-16 23:54:27 +0000

    fix: Inhibition of exclusive sub-commands + adjust for language changes.

--- src/inhibitors/ExclusiveCommandInhibitor.ts
+++ src/inhibitors/ExclusiveCommandInhibitor.ts
@@ -1,5 +1,6 @@
 import { Inhibitor, InhibitorStore, KlasaMessage, Command } from 'klasa'
 import ExclusiveCommand from '@libraries/ExclusiveCommand'
+import { getSubCommands } from '@utilities/commands'
 
 export default class extends Inhibitor {
   constructor(store: InhibitorStore, file: string[], directory: string) {
@@ -20,11 +21,22 @@ export default class extends Inhibitor {
     }
 
     if (!command.exclusive.guilds.includes(message.guild.id)) {
-      if (command.subcommands) {
-        console.log('!!!', command.usage)
+      if (command.subcommands && command.exclusive.subcommands.length) {
+        const subcommand = message.args[0]
+
+        if (command.exclusive.subcommands.some(name => name === subcommand)) {
+          return message.language.get(
+            'INHIBITOR_EXCLUSIVE_SUBCOMMAND_NOTICE',
+            subcommand,
+            command.exclusive.guilds.map(
+              guildId => this.client.guilds.get(guildId).name
+            )
+          )
+        }
       } else {
         return message.language.get(
           'INHIBITOR_EXCLUSIVE_COMMAND_NOTICE',
+          message.commandText,
           command.exclusive.guilds.map(
             guildId => this.client.guilds.get(guildId).name
           )
