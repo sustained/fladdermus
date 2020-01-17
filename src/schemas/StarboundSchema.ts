591a7cce4d57ad2f373264f720097d76f2db5e3a
sustained <sustained.dissonance@gmail.com>
2020-01-17 08:15:23 +0000

    other: Update schema.

--- src/schemas/StarboundSchema.ts
+++ src/schemas/StarboundSchema.ts
@@ -7,6 +7,7 @@ Client.defaultUserSchema.add('starbound', folder => {
     .add('accountName', 'string')
     .add('statistics', stats => {
       return stats
+        .add('lastLogin', 'integer', { configurable: false, default: 0 })
         .add('totalLogins', 'integer', { configurable: false, default: 0 })
         .add('minutesPlayed', 'integer', { configurable: false, default: 0 })
     })
@@ -16,5 +17,10 @@ Client.defaultClientSchema.add('starbound', starbound => {
   return starbound
     .add('autoStart', 'boolean', { default: false })
     .add('autoRestart', 'boolean', { default: false })
+    .add('enabledEvents', 'string', {
+      default: ['join', 'part', 'chat', 'start', 'stop'],
+      array: true,
+    })
+    .add('queueMessages', 'boolean', { default: false })
     .add('authorisedUsers', 'user', { array: true })
 })
